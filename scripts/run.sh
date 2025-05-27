eb7c5ac3c7a8d0507348100e3b381a7495eb8ba2
murphy-cthsu <murphy.cthsu@gmail.com>
2025-05-27 16:34:28 +0800

    update readme and script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,15 @@
-python src/training_new.py \
-    --dataset "user_session_data" \
-    --lambda_V 0.1 \
-    --data_path "./data" \
-    --pretrained_path "./pretrained" \
-    --model_name "Qwen/Qwen3-1.7B" 
+for i in {0..9}
+do
+    python src/training_new.py \
+        --dataset "user_session_data_${i}" \
+        --lambda_V 0.1 \
+        --data_path "./data" \
+        --pretrained_path "./pretrained" \
+        --model_name "Qwen/Qwen3-1.7B"
 
-python src/finetuning_new.py \
-    --dataset "user_session_data" \
-    --lambda_V 0.1 \
-    --model_path "models"\
-    --model_name "Qwen/Qwen3-1.7B" 
+    python src/finetuning_new.py \
+        --dataset "user_session_data_${i}" \
+        --lambda_V 0.1 \
+        --model_path "models" \
+        --model_name "Qwen/Qwen3-1.7B"
+done
